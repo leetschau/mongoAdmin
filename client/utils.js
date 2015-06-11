6b2e9892131081417a7b09685baaa0de76eac2e2
leetschau <leetschau@gmail.com>
2015-06-11 10:25:34 +0800

    add response for update result (success/error) function
    Signed-off-by: leetschau <leetschau@gmail.com>

--- client/utils.js
+++ client/utils.js
@@ -1,3 +1,13 @@
 String.prototype.toTitleCase = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+AutoForm.addHooks(null, {
+  onSuccess: function(operation, result, template) {
+    FlashMessages.sendSuccess('Update Success!', { autoHide: true, hideDelay: 1000 });
+    Router.go("/");
+  },
+  onError: function(formType, error) {
+    FlashMessages.sendError(error, { autoHide: true, hideDelay: 1000 });
+  }
+});
